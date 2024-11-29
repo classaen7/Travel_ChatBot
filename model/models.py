6b4debc122b4aa03af13b8f86c4473156070c962
classaen7 <classaen7@naver.com>
2024-11-30 01:06:52 +0900

    fix: models

--- model/models.py
+++ model/models.py
@@ -1,13 +1,14 @@
 # import spacy
 from sentence_transformers import SentenceTransformer
 import semchunk
-chunker = semchunk.chunkerify('gpt-4', 128)
+
+# chunker = semchunk.chunkerify('gpt-4', 128)
 
 def init_models(spc = "ko_core_news_md", stf = 'all-MiniLM-L6-v2'):
     
     # nlp = spacy.load(spc)
 
-    nlp = semchunk.chunkerify('gpt-4', 128)
+    nlp = semchunk.chunkerify('gpt-4', 64)
     embedder = SentenceTransformer(stf)
 
     print("모든 모델 초기화 완료!")
