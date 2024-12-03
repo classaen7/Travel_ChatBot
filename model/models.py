5efe175ea40278146f163bee95a450e9b962cc22
classaen7 <classaen7@naver.com>
2024-12-03 22:39:22 +0900

    change model

--- model/models.py
+++ model/models.py
@@ -4,11 +4,11 @@ import semchunk
 
 # chunker = semchunk.chunkerify('gpt-4', 128)
 
-def init_models(spc = "ko_core_news_md", stf = 'all-MiniLM-L6-v2'):
+def init_models(spc = "ko_core_news_md", stf = 'nlpai-lab/KoE5'):
     
     # nlp = spacy.load(spc)
 
-    nlp = semchunk.chunkerify('gpt-4', 64)
+    nlp = semchunk.chunkerify('nlpai-lab/KoE5', 128)
     embedder = SentenceTransformer(stf)
 
     return nlp, embedder
