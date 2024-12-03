a5e5fa6e70402a5f3189697ff45b1fa899ad41b6
classaen7 <classaen7@naver.com>
2024-12-03 22:39:03 +0900

    token_summary

--- RAG/summary_history.py
+++ RAG/summary_history.py
@@ -2,15 +2,38 @@ import spacy
 import pytextrank
 
 # spaCy 모델 로드
-nlp = spacy.load("ko_core_news_md")
+nlp = spacy.load("ko_core_news_sm")
 
 # PyTextRank를 파이프라인에 추가
 nlp.add_pipe("textrank")
 
 
-def summary_text(hist, limit_phrases=15, limit_sentences=3):
-    doc = nlp(hist)
-    result = [
-        sent.text for sent in doc._.textrank.summary(limit_phrases=15, limit_sentences=3)
+def summary_text(hist):
+    assistant_hist = []
+    user_hist = []
+
+    for msg in hist:
+        if msg["role"] == "assistant":
+            assistant_hist.append(msg["content"])
+        
+        elif msg["role"] == "user":
+            user_hist.append(msg["content"])
+
+    assistant_text = ' '.join(assistant_hist)
+    user_text = ' '.join(user_hist)
+
+    a_doc = nlp(assistant_text)
+    u_doc = nlp(user_text)
+
+    a_result = [
+        sent.text for sent in a_doc._.textrank.summary(limit_phrases=15, limit_sentences=3)
     ]
-    return ' '.join(result)
+
+    u_result = [
+        sent.text for sent in u_doc._.textrank.summary(limit_phrases=15, limit_sentences=3)
+    ]
+
+    print(a_result)
+    print(u_result)
+
+    return ' '.join(a_result), ' '.join(u_result)
